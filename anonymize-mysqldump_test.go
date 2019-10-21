86a0483847937e886d63785b73fb17648cb0b610
Nathaniel Schweinberg <natanielks@gmail.com>
2019-10-21 18:51:28 -0500

    add a fake data library which can be seeded for deterministic testing

--- anonymize-mysqldump_test.go
+++ anonymize-mysqldump_test.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"fmt"
+	"syreclabs.com/go/faker"
 	"testing"
 )
 
 var (
 	usersQuery              = "INSERT INTO `wp_users` VALUES (1,'username','user_pass','username','hosting@humanmade.com','','2019-06-12 00:59:19','',0,'username'),(2,'username','user_pass','username','hosting@humanmade.com','http://notreal.com/username','2019-06-12 00:59:19','',0,'username');"
-	usersQueryRecompiled    = "insert into wp_users values (1, 'foobar', 'foobar', 'foobar', 'foobar@example.com', '', '2019-06-12 00:59:19', '', 0, 'Ashley Jones'), (2, 'foobar', 'foobar', 'foobar', 'foobar@example.com', 'https://example.com', '2019-06-12 00:59:19', '', 0, 'Ashley Jones');"
+	usersQueryRecompiled    = "insert into wp_users values (1, 'treva_cremin', 'NjaK5HeMAMuv', 'hailey', 'bernice.heaney@example.net', '', '2019-06-12 00:59:19', '', 0, 'Kylie Rice'), (2, 'eduardo', 'J3JRQ4XoIxXX6A', 'albert.okeefe', 'brooke.hayes@example.net', 'http://pfannerstill.net/brando', '2019-06-12 00:59:19', '', 0, 'Ardella Jenkins PhD');"
 	userMetaQuery           = "INSERT INTO `wp_usermeta` VALUES (1,1,'first_name','John'),(2,1,'last_name','Doe'),(3,1,'foobar','bazquz'),(4,1,'nickname','Jim'),(5,1,'description','Lorum ipsum.'),(6,2,'first_name','Janet'),(7,2,'last_name','Doe'),(8,2,'foobar','bazquz'),(9,2,'nickname','Jane'),(10,2,'description','Lorum ipsum.');"
-	userMetaQueryRecompiled = "insert into wp_usermeta values (1, 1, 'first_name', 'Ashley'), (2, 1, 'last_name', 'Jones'), (3, 1, 'foobar', 'bazquz'), (4, 1, 'nickname', 'Ashley'), (5, 1, 'description', 'Foo bar baz quz.'), (6, 2, 'first_name', 'Ashley'), (7, 2, 'last_name', 'Jones'), (8, 2, 'foobar', 'bazquz'), (9, 2, 'nickname', 'Ashley'), (10, 2, 'description', 'Foo bar baz quz.');"
+	userMetaQueryRecompiled = "insert into wp_usermeta values (1, 1, 'first_name', 'Stephania'), (2, 1, 'last_name', 'Hamill'), (3, 1, 'foobar', 'bazquz'), (4, 1, 'nickname', 'Noah'), (5, 1, 'description', 'Dolorum nostrum alias.'), (6, 2, 'first_name', 'Ed'), (7, 2, 'last_name', 'Koelpin'), (8, 2, 'foobar', 'bazquz'), (9, 2, 'nickname', 'Watson'), (10, 2, 'description', 'Qui voluptatum est.');"
 	commentsQuery           = "INSERT INTO `wp_comments` VALUES (1,1,'A WordPress Commenter','wapuu@wordpress.example','https://wordpress.org/','','2019-06-12 00:59:19','2019-06-12 00:59:19','Hi, this is a comment.\nTo get started with moderating, editing, and deleting comments, please visit the Comments screen in the dashboard.\nCommenter avatars come from <a href=\"https://gravatar.com\">Gravatar</a>.',0,'1','','',0,0);"
-	commentsQueryRecompiled = "insert into wp_comments values (1, 1, 'foobar', 'foobar@example.com', 'https://example.com', '', '2019-06-12 00:59:19', '2019-06-12 00:59:19', 'Hi, this is a comment.\\nTo get started with moderating, editing, and deleting comments, please visit the Comments screen in the dashboard.\\nCommenter avatars come from <a href=\\\"https://gravatar.com\\\">Gravatar</a>.', 0, '1', '', '', 0, 0);"
+	// Don't forget to escape \ because it'll translate to a newline and not pass
+	// the comparison test
+	commentsQueryRecompiled = "insert into wp_comments values (1, 1, 'kamren.ohara', 'michele_barton@example.net', 'http://ebert.com/korey_keeling', '', '2019-06-12 00:59:19', '2019-06-12 00:59:19', 'Hi, this is a comment.\\nTo get started with moderating, editing, and deleting comments, please visit the Comments screen in the dashboard.\\nCommenter avatars come from <a href=\\\"https://gravatar.com\\\">Gravatar</a>.', 0, '1', '', '', 0, 0);"
 )
+
+func init() {
+	faker.Seed(432)
+}
 
 func TestApplyConfigToQuery(t *testing.T) {
 
--- transformation-functions.go
+++ transformation-functions.go
@@ -2,41 +2,42 @@ package main
 
 import (
 	"github.com/xwb1989/sqlparser"
+	"syreclabs.com/go/faker"
 )
 
 func generateUsername(value *sqlparser.SQLVal) *sqlparser.SQLVal {
-	return sqlparser.NewStrVal([]byte("foobar"))
+	return sqlparser.NewStrVal([]byte(faker.Internet().UserName()))
 }
 
 func generatePassword(value *sqlparser.SQLVal) *sqlparser.SQLVal {
 	// TODO encrypt this value
-	return sqlparser.NewStrVal([]byte("foobar"))
+	return sqlparser.NewStrVal([]byte(faker.Internet().Password(8, 14)))
 }
 
 func generateEmail(value *sqlparser.SQLVal) *sqlparser.SQLVal {
-	return sqlparser.NewStrVal([]byte("foobar@example.com"))
+	return sqlparser.NewStrVal([]byte(faker.Internet().SafeEmail()))
 }
 
 func generateURL(value *sqlparser.SQLVal) *sqlparser.SQLVal {
-	return sqlparser.NewStrVal([]byte("https://example.com"))
+	return sqlparser.NewStrVal([]byte(faker.Internet().Url()))
 }
 
 func generateName(value *sqlparser.SQLVal) *sqlparser.SQLVal {
-	return sqlparser.NewStrVal([]byte("Ashley Jones"))
+	return sqlparser.NewStrVal([]byte(faker.Name().Name()))
 }
 
 func generateFirstName(value *sqlparser.SQLVal) *sqlparser.SQLVal {
-	return sqlparser.NewStrVal([]byte("Ashley"))
+	return sqlparser.NewStrVal([]byte(faker.Name().FirstName()))
 }
 
 func generateLastName(value *sqlparser.SQLVal) *sqlparser.SQLVal {
-	return sqlparser.NewStrVal([]byte("Jones"))
+	return sqlparser.NewStrVal([]byte(faker.Name().LastName()))
 }
 
 func generateParagraph(value *sqlparser.SQLVal) *sqlparser.SQLVal {
-	return sqlparser.NewStrVal([]byte("Foo bar baz quz."))
+	return sqlparser.NewStrVal([]byte(faker.Lorem().Sentence(3)))
 }
 
 func generateIPv4(value *sqlparser.SQLVal) *sqlparser.SQLVal {
-	return sqlparser.NewStrVal([]byte("12.34.56.78"))
+	return sqlparser.NewStrVal([]byte(faker.Internet().IpV4Address()))
 }
