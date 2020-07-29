888ca784f64d88c6ae37522340ed95fff852783b
Pedro <pedrogomesyoo@gmail.com>
2020-07-29 16:52:01 +0700

    skip tests

--- transformation-functions.go
+++ transformation-functions.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"github.com/omnea/faker"
 	"github.com/xwb1989/sqlparser"
-	"syreclabs.com/go/faker"
+	"math/rand"
+	"strings"
 )
 
 func generateUsername(value *sqlparser.SQLVal) *sqlparser.SQLVal {
@@ -38,6 +41,98 @@ func generateParagraph(value *sqlparser.SQLVal) *sqlparser.SQLVal {
 	return sqlparser.NewStrVal([]byte(faker.Lorem().Sentence(3)))
 }
 
+func generateCustomTitle(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte(faker.Lorem().Word()))
+}
+
 func generateIPv4(value *sqlparser.SQLVal) *sqlparser.SQLVal {
 	return sqlparser.NewStrVal([]byte(faker.Internet().IpV4Address()))
+}
+
+func generateCustomPhoneNumber(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte(fmt.Sprintf("0%v", faker.Number().Number(rand.Intn(12-7)+7))))
+}
+
+func generateCustomUserAgent(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"))
+}
+
+func generateLibPrefix(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte(faker.Name().Prefix()))
+}
+
+func generateLibCompanyName(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte(faker.Company().Name()))
+}
+
+func generateLibStreet(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte(faker.Address().StreetName()))
+}
+
+func generateCustomStreet(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte(faker.Address().StreetName()))
+}
+
+func generateLibBuildingNumber(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte(faker.Address().BuildingNumber()))
+}
+
+func generateLibAdditionalAddress(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte(faker.Address().SecondaryAddress()))
+}
+
+func generateLibCity(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte(faker.Address().City()))
+}
+
+func generateLibZip(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte(faker.Address().ZipCode()))
+}
+
+func generateLibState(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte(faker.Address().State()))
+}
+
+func generateLibCountry(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte(faker.Address().Country()))
+}
+
+func generateCustomCountry(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte("Deutschland"))
+}
+
+func generateLibParagraph(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte(faker.Lorem().Paragraph(3)))
+}
+
+func generateCustomSocialNetwork(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte(faker.RandomChoice([]string{"Google", "Facebook"})))
+}
+
+func generateCustomSocialId(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte("REDACTED"))
+}
+
+func generateCustomSocialToken(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte("REDACTED"))
+}
+
+func generateLibInternetUser(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte(faker.Internet().UserName()))
+}
+
+func generateCustomUniqueUser(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte(fmt.Sprintf("%v.%v", faker.RandomString(16), faker.Internet().SafeEmail())))
+}
+
+func generateCustomPassword(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte(strings.ToLower(faker.Internet().Password(64, 64))))
+}
+
+func generateCustomRecoverToken(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte(faker.Internet().Password(30, 30)))
+}
+
+func generateCustomUserToken(value *sqlparser.SQLVal) *sqlparser.SQLVal {
+	return sqlparser.NewStrVal([]byte(faker.Internet().Password(8, 20)))
 }
